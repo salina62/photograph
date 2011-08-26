1087c8a7d2024ba126f057322782c94a81ca1803
salina acharya <s0201016@cqu.edu.au>
2011-08-27 00:30:43 +1000

    added 20 photos to the  photo scaffold to display in the web.

---
+++ db/seeds.rb
@@ -0,0 +1,319 @@
+photo.delete_all
+photo.create(:title => 'Giant Mushroom',
+:Description =>
+%{<p>
+ A picture of a giant mushroom look like beach shade.
+very interesting and amazingly photographed with a perfect
+colour contrast.
+</p>},
+  :Location => 'Sa Coma, Balearic Island, ES' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-05-13 07:06:00 UTC' ,
+  :Photographer_name => 'The LIbrary of Congress' ,
+:image_url => '/images/sampleImage1.jpg' ,
+:single_price => 10.00 ,
+:outright_price => 100.00)
+
+
+# . ..
+photo.create(:title => 'Lincluden Abbey',
+:Description =>
+%{<p>
+a very amazing views of landscape and architecture in Scotland.
+this amazing picture was taken some time in 1890.
+</p>},
+  :Location => 'Dumfries, Scotland' ,
+  :Date_and_time.to_formatted_s(:db) => '2006-02-11 06:27:00 UTC' ,
+  :Photographer_name => 'Philip Klinger' ,
+:image_url => '/images/sampleImage2.jpg' ,
+:single_price => 11.00 ,
+:outright_price => 110.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Four of a Kind',
+:Description =>
+%{<p>
+ a picture of very cute goats with the view od South PArk and the
+Collegiates in the distance. remarkably cute picture.
+</p>},
+  :Location => 'Colorado, USA' ,
+  :Date_and_time.to_formatted_s(:db) => '2010-08-11 01:30:00 UTC' ,
+  :Photographer_name => 'Sandra Leidholdt' ,
+:image_url => '/images/sampleImage3.jpg' ,
+:single_price => 12.00 ,
+:outright_price => 120.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'New York, Times Square -Busy Crossing',
+:Description =>
+%{<p>
+ a picture revealing how busy new york city is. the photo
+combines two shot in photoshop by manual exposure blending,
+cross processing and tonal contrast from color Efex.
+</p>},
+  :Location => 'New York. Times Square, USA' ,
+  :Date_and_time.to_formatted_s(:db) => '2009-06-16 21:44:00 UTC' ,
+  :Photographer_name => 'Philip Klinger' ,
+:image_url => '/images/sampleImage4.jpg' ,
+:single_price => 13.00 ,
+:outright_price => 130.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Young Hyena',
+:Description =>
+%{<p>
+ a picture of a young hyena who was nervous to give a shot and turned back
+before the photographer could click it picture. However, the photographer
+could click the picture of the hyena in its alert position
+which is the beauty of this picture.
+</p>},
+  :Location => 'Mpumalanga, ZA' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-08-21 03:37:00 UTC' ,
+  :Photographer_name => 'Piet Grobler' ,
+:image_url => '/images/sampleImage5.jpg' ,
+:single_price => 9.00 ,
+:outright_price => 90.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Stromy Night at Horeshoe Bend',
+:Description =>
+%{<p>
+ a beautiful picture from nothern arizona clicked just as the wind started
+blowing with a definite treat of a sunset.
+</p>},
+  :Location => 'Page, Arizona , US' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-08-18 05:30:00 UTC' ,
+  :Photographer_name => 'Aaron M' ,
+:image_url => '/images/sampleImage6.jpg' ,
+:single_price => 11.00 ,
+:outright_price => 110.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Nightingale',
+:Description =>
+%{<p>
+ a very cute and funny picture of a nightingale with blured view at
+the distance and a clear vegetation.
+</p>},
+  :Location => 'Draper Villas, La Jolla, CA, US' ,
+  :Date_and_time.to_formatted_s(:db) => '2010-06-05 05:27:00 UTC' ,
+  :Photographer_name => 'Rudiger' ,
+:image_url => '/images/sampleImage7.jpg' ,
+:single_price => 10.00 ,
+:outright_price => 100.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Santa Monica Pier',
+:Description =>
+%{<p>
+ a very wonderful picture of Santa Monica with mind blowing nightview
+</p>},
+  :Location => 'Santa Monica, Los Angeles,California USA' ,
+  :Date_and_time.to_formatted_s(:db) => '2009-10-23 08:27:00 UTC' ,
+  :Photographer_name => 'Rudiger' ,
+:image_url => '/images/sampleImage9.jpg' ,
+:single_price => 15.00 ,
+:outright_price => 150.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'dew drop',
+:Description =>
+%{<p>
+ a phenominal picture of a dew drop with a reflection of a
+purple flower. this flower is blurred at the back and is
+crystal clear inside the dew drop. this is the best shot
+of the picture.
+</p>},
+  :Location => 'Broicher, Siedhung, North Rhine-Westphalia, DE' ,
+  :Date_and_time.to_formatted_s(:db) => '2009-04-11 06:15:00 UTC' ,
+  :Photographer_name => 'Rudiger' ,
+:image_url => '/images/sampleImage10.jpg' ,
+:single_price => 16.00 ,
+:outright_price => 160.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Dusk',
+:Description =>
+%{<p>
+ an excellent picture of the road with beautiful texture and a
+yellow lines that can take one ot the beautiful hills at the distance .
+
+</p>},
+  :Location => 'Sky Valley, California, USA' ,
+  :Date_and_time.to_formatted_s(:db) => '2007-01-05 17:17:00 UTC' ,
+  :Photographer_name => 'Rudiger' ,
+:image_url => '/images/sampleImage11.jpg' ,
+:single_price => 10.00 ,
+:outright_price => 100.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Seattle At NIght',
+:Description =>
+%{<p>
+ breathtaking picture of nighttime skyline. overall outstanding capture
+of Seattle city.
+</p>},
+  :Location => 'Seattle, WA, USA' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-08-16 21:27:00 UTC' ,
+  :Photographer_name => 'name not available' ,
+:image_url => '/images/sampleImage12.jpg' ,
+:single_price => 12.00 ,
+:outright_price => 120.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Mandelabridge Zoetermeer',
+:Description =>
+%{<p>
+ a very interesting picture of namdelabridge. which
+also has been awarded as the interesting picture of
+the month. the picture has been photographed in a
+very perfect perspectives.
+</p>},
+  :Location => 'Zoetermeer, South Holland, NL' ,
+  :Date_and_time.to_formatted_s(:db) => '2006-07-06 14:37:00 UTC' ,
+  :Photographer_name => 'Kees de Vos' ,
+:image_url => '/images/sampleImage13.jpg' ,
+:single_price => 12.00 ,
+:outright_price => 120.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Canopus1',
+:Description =>
+%{<p>
+  the picture is the great presentation of Villa Adriana.in this picture one can
+ imagine Hadrian ot one of his courtiers strolling across the scene with nice
+reflection and composition.
+</p>},
+  :Location => 'Sa Coma, Balearic Island, ES' ,
+  :Date_and_time.to_formatted_s(:db) => '2006-07-31 13:27:00 UTC' ,
+  :Photographer_name => 'Richard White' ,
+:image_url => '/images/sampleImage14.jpg' ,
+:single_price => 15.00 ,
+:outright_price => 150.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Garade Liege - Guillemins',
+:Description =>
+%{<p>
+ a picture of a Calatrava buildings with an amazing
+composition.
+</p>},
+  :Location => 'Fragne, Liege, BE' ,
+  :Date_and_time.to_formatted_s(:db) => '2008-05-10 16:56:00 UTC' ,
+  :Photographer_name => 'Kristo' ,
+:image_url => '/images/sampleImage15.jpg' ,
+:single_price => 10.00 ,
+:outright_price => 100.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Stream',
+:Description =>
+%{<p>
+ a picture of a stream of Oregon coast with beautiful
+waves. looks almost like computer generated. very phenominal
+photography.
+</p>},
+  :Location => 'Lane Country, Oregon, US' ,
+  :Date_and_time.to_formatted_s(:db) => '2006-06-11 17:27:00 UTC' ,
+  :Photographer_name => 'AJ Schuster' ,
+:image_url => '/images/sampleImage16.jpg' ,
+:single_price => 15.00 ,
+:outright_price => 150.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Split light',
+:Description =>
+%{<p>
+ a picture in which one world meets another. this picture has
+captured a beautiful mystical moment that makes people
+look at it every time. very beautiful
+</p>},
+  :Location => 'Jefferson Country, Washington, US' ,
+  :Date_and_time.to_formatted_s(:db) => '2006-07-23 18:27:00 UTC' ,
+  :Photographer_name => 'eden' ,
+:image_url => '/images/sampleImage17.jpg' ,
+:single_price => 15.00 ,
+:outright_price => 150.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Lake Wanaka',
+:Description =>
+%{<p>
+ a picture of lake wanaka with lovely reflection of trees
+that gives a surreal feel. a very well composition
+</p>},
+  :Location => 'Wanaka, Otago, NZ' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-04-21 17:17:00 UTC' ,
+  :Photographer_name => 'Karen' ,
+:image_url => '/images/sampleImage18.jpg' ,
+:single_price => 11.00 ,
+:outright_price => 110.00)
+
+# . ..
+
+# . ..
+photo.create(:title => 'Explored',
+:Description =>
+%{<p>
+ The rhythm of my footsteps crossing flatlands
+to your door have been silenced forever more.
+The distance is quite simply much too far for me to row.
+</p>},
+  :Location => 'No location available' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-08-07 17:27:00 UTC' ,
+  :Photographer_name => 'kelley Leigh' ,
+:image_url => '/images/sampleImage19.jpg' ,
+:single_price => 9.00 ,
+:outright_price => 90.00)
+
+# . ..
+
+
+
+# . ..
+photo.create(:title => 'Lake Wanaka',
+:Description =>
+%{<p>
+ a picture of lake Wanaka. the picture is very artistic with
+fantastic colours presenting incredible beauty of a beautiful
+scene.in one word, simply spectacular.
+</p>},
+  :Location => 'Wanaka, Otago, NZ' ,
+  :Date_and_time.to_formatted_s(:db) => '2011-05-11 16:17:00 UTC' ,
+  :Photographer_name => 'Karen' ,
+:image_url => '/images/sampleImage20.jpg' ,
+:single_price => 16.00 ,
+:outright_price => 160.00)
+
+# . ..
+
+
