b5095c5b80e276210d75e593852a813a51db3e72
salina acharya <s0201016@cqu.edu.au>
2011-10-04 21:12:51 +1000

    added the validation for the mainuser

---
+++ app/models/mainuser.rb
@@ -0,0 +1,15 @@
+class Mainuser < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+  validates :password, :confirmation => true
+  attr_accessor :password_confirmation
+  attr_reader :password
+  validate :password_must_be_present
+
+  def User.encrypt_password(password, salt)
+    Digest::SHA2.hexdigest(password + "wibble" + salt)
+  end
+  private
+    def password_must_be_present
+      errors.add(:password, "Missing password") unless hashed_password.present?
+    end
+end
